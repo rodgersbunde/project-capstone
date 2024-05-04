424a145e2d62302f1399b2ae2aa3f9fcd725f92e
Rodgers <roggybunde@gmail.com>
2024-05-04 13:59:59 +0300

    final commit

--- recipe.py
+++ recipe.py
@@ -3,28 +3,10 @@ from surprise import Dataset, Reader, SVD
 import pandas as pd
 
 # Load the dataset
-dt = pd.read_csv('final_data.csv')
+df = pd.read_csv('final_data.csv')
 
 # Define Streamlit app content
 def streamlit_app():
-
-    # Load collaborative filtering model
-    reader = Reader(rating_scale=(0, 100))
-    data = Dataset.load_from_df(dt[['user_id', 'recipe_code', 'ratings']], reader)
-    trainset = data.build_full_trainset()
-    algo = SVD()
-    algo.fit(trainset)
-
-    # Function to get recommendations for a user
-    def get_recommendations(user_id, dt):
-        user_recipe = dt[dt['user_id'] == user_id]['recipe_code'].unique()
-        recommended_recipe = []
-        for recipe_code in dt['recipe_code'].unique():
-            if recipe_code not in user_recipe:
-                predicted_ratings = algo.predict(user_id, recipe_code).est
-                recommended_recipe.append((recipe_code, predicted_ratings))
-        recommended_recipe.sort(key=lambda x: x[1], reverse=True)
-        return recommended_recipe
     # Set page width and background color
     st.set_page_config(page_title="Recipe Recommendation App", layout="wide", page_icon="🥘", initial_sidebar_state="expanded")
 
@@ -58,33 +40,47 @@ def streamlit_app():
     elif page == 'Results':
         st.title('Recipe Search Results')
 
+        # Load collaborative filtering model
+        reader = Reader(rating_scale=(0, 100))
+        data = Dataset.load_from_df(df[['user_id', 'recipe_code', 'ratings']], reader)
+        trainset = data.build_full_trainset()
+        algo = SVD()
+        algo.fit(trainset)
+
+        # Function to get recommendations for a user
+        def get_recommendations(user_id, df):
+            user_recipe = df[df['user_id'] == user_id]['recipe_code'].unique()
+            recommended_recipe = []
+            for recipe_code in df['recipe_code'].unique():
+                if recipe_code not in user_recipe:
+                    predicted_ratings = algo.predict(user_id, recipe_code).est
+                    recommended_recipe.append((recipe_code, predicted_ratings))
+            recommended_recipe.sort(key=lambda x: x[1], reverse=True)
+            return recommended_recipe
+
         # Add text inputs for entering user ID and recipe name
         user_id = st.text_input('Enter User ID:')
         recipe_name = st.text_input('Enter Recipe Name:')
 
         # Get recommendations for the user
         if user_id and recipe_name:
-            recommended_recipe = get_recommendations(int(user_id), dt)
+            recommended_recipe = get_recommendations(int(user_id), df)
             for recipe_code, predicted_rating in recommended_recipe[:10]:
-                recipe_name = dt[dt['recipe_code'] == recipe_code]['recipe_name'].iloc[0]
-                ingredients = dt[dt['recipe_code'] == recipe_code]['ingredients'].iloc[0]
+                recipe_name = df[df['recipe_code'] == recipe_code]['recipe_name'].iloc[0]
+                ingredients = df[df['recipe_code'] == recipe_code]['ingredients'].iloc[0]
                 # Check if cooking instructions exist before accessing them
-        if not dt[dt['recipe_code'] == recipe_code]['cooking_instructions'].empty:
-            cooking_instructions = dt[dt['recipe_code'] == recipe_code]['cooking_instructions'].iloc[0]
-            st.write(f"Recipe Name: {recipe_name}")
-            st.write(f"Predicted Rating: {predicted_rating}")
-            st.write(f"Ingredients: {ingredients}")
-            st.write(f"Cooking Instructions: {cooking_instructions}")
-        else:
-            st.write(f"Recipe Name: {recipe_name}")
-            st.write(f"Predicted Rating: {predicted_rating}")
-            st.write(f"Ingredients: {ingredients}")
-            st.write("Cooking Instructions not available for this recipe.")
-                
+                if 'cooking_instructions' in df.columns and not df[df['recipe_code'] == recipe_code]['cooking_instructions'].empty:
+                    cooking_instructions = df[df['recipe_code'] == recipe_code]['cooking_instructions'].iloc[0]
+                    st.write(f"Recipe Name: {recipe_name}")
+                    st.write(f"Predicted Rating: {predicted_rating}")
+                    st.write(f"Ingredients: {ingredients}")
+                    st.write(f"Cooking Instructions: {cooking_instructions}")
+                else:
+                    st.write(f"Recipe Name: {recipe_name}")
+                    st.write(f"Predicted Rating: {predicted_rating}")
+                    st.write(f"Ingredients: {ingredients}")
+                    st.write("Cooking Instructions not available for this recipe.")
+
 # Run the Streamlit app
 if __name__ == "__main__":
     streamlit_app()
-
-    
-    
-    
